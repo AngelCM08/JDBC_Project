f5e574fc1f52e24460e1d9b8b7c86084058e49d0
AngelCM08 <92088141+AngelCM08@users.noreply.github.com>
2023-01-31 16:14:52 +0100

    Add files via upload

--- src/action/Actions.java
+++ src/action/Actions.java
@@ -39,7 +39,7 @@ public class Actions {
         List<String[]> data = GetDataFromCSV(tabla);
 
         try {
-            String query = "SELECT column_name, data_type FROM information_schema.columns WHERE table_name = ? ORDER BY ordinal_position;";
+            String query = "SELECT column_name, data_type FROM information_schema.columns WHERE table_name = ?;";
             PreparedStatement ps_header = c.prepareStatement(query);
             ps_header.setString(1,tabla);
             ResultSet resultSet = ps_header.executeQuery();
@@ -48,8 +48,7 @@ public class Actions {
                 header.add(resultSet.getString(1));
                 data_types.add(resultSet.getString(2));
             }
-            System.out.println(Arrays.toString(header.toArray()));
-            System.out.println(Arrays.toString(data_types.toArray()));
+            data_types.forEach(System.out::println);
 
             for (int i = 0; i < header.size(); i++) {
                 if(i == 0) values = values.concat("(?, ");
@@ -60,9 +59,8 @@ public class Actions {
             String insert = "INSERT INTO "+tabla+"("+header.toString().substring(1,header.toString().length()-1)+") VALUES "+values+";";
             PreparedStatement ps_insert = c.prepareStatement(insert);
 
-            for (int i = 1; i <= header.size(); i++) {
+            for (int i = 0; i < data.size(); i++) {
                 String[] fields = data.get(i);
-                System.out.println(Arrays.toString(fields));
                 try {
                     for (int j = 0; j < fields.length; j++) {
                         if(data_types.get(j).equals("integer")){
