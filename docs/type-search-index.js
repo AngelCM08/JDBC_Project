c84b5d70018e7cad140000a5dbca04c798c69f45
Ángel <angelcastromerino@gmail.com>
2023-02-09 17:23:57 +0100

    JavaDoc

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"connection","l":"ConnectionFactory"},{"p":"actions","l":"DB_Actions"},{"p":"actions","l":"DB_Deletes"},{"p":"actions","l":"DB_Inserts"},{"p":"actions","l":"DB_Selects"},{"p":"actions","l":"DB_Updates"},{"p":"<Unnamed>","l":"JDBC_Main"},{"p":"ui","l":"JDBC_Menu"}];updateSearchResults();
